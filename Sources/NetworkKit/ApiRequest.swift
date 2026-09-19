ce7e4dd7723650d72628e92d5b9c4b09f438d3ea
Omar Hernandez Gonzalez <omaglz.92@gmail.com>
2026-09-18 21:46:27 -0600

    header duplicado

--- Sources/NetworkKit/ApiRequest.swift
+++ Sources/NetworkKit/ApiRequest.swift
@@ -143,7 +143,6 @@ extension ApiRequest {
     
     static var baseHeaders: [ApiHeaderKey: String] {
         [
-            .contentType: ApiHeaderValue.textPlain,
             .accept: ApiHeaderValue.jsonText,
             .acceptEncoding: ApiHeaderValue.gzipDeflateBr,
             .connection: ApiHeaderValue.keepAlive,
